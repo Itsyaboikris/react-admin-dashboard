e294ce741d2d471af834d5f7879c1655d6a49d0d
itsyaboikris <kristoff_riley@outlook.com>
2021-08-05 16:34:27 -0400

    created Chart component

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./home.css";
 import FeaturedInfo from "../../featuredInfo/FeaturedInfo";
+import Chart from "../../chart/Chart";
+import {data} from "../../dummyData";
+
 
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={data} title="User Analytics" grid dataKey="Active Users"/>
         </div>
     )
 }
