6ae076faceac1438add4cb3de60de5c9a232e627
itsyaboikris <kristoff_riley@outlook.com>
2021-08-05 09:45:15 -0400

    Sidebar component + styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Topbar from "./components/topbar/Topbar";
+import React from "react";
+import SideBar from "./components/sidebar/Sidebar";
+import "./app.css";
 
 function App() {
   return (
     <div>
         <Topbar/>
-      <h1>Hello</h1>
-        hello
+        <div className="container">
+            <SideBar/>
+            <div className="other">
+                other pages
+            </div>
+        </div>
     </div>
   );
 }
